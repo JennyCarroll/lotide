605aa954117315376d13bc4e5d270c6ce0824f31
Jenny Carroll <jennygc@telus.net>
2023-02-07 02:45:11 +0000

    I refactored my assertEqual function to remove an if statement that was unnecessary

--- head.js
+++ head.js
@@ -1,10 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return
   }
-  if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
 };
 const head = function(array) {
   return array[0];
