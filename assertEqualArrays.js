0407da633a66447cef2f6fe554d4dabae32bb588
Jenny Carroll <jennygc@telus.net>
2023-02-08 02:30:57 +0000

    created an assertEqualArray that works with arrays and uses my eqArrays function.  also fixed a bug in my eqArrays function dealing with length of the arrays

---
+++ assertEqualArrays.js
@@ -0,0 +1,19 @@
+function eqArrays(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false
+  }
+  for (let i = 0; i < array1.length; ++i) {
+    if (array1[i] !== array2[i]) {
+      return false
+    }
+    }
+    return true
+  }
+
+
+const assertEqualArrays = function(actual, expected) {
+  eqArrays(actual, expected) ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+}
+
+assertEqualArrays(["Ariel", "Jenny"], ["Ariel", "Jenny", "Alykhan"]) /* should fail*/
+assertEqualArrays(["Ariel", "Jenny"], ["Ariel", "Jenny", "Alykhan"]) /* should pass*/
