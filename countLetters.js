1fc9adcba16663a3384497b307945abe2344e577
Jenny Carroll <jennygc@telus.net>
2023-02-08 23:31:54 +0000

    I created a function that accepts a string and outputs an object showing how many times each letter was encountered

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  actual !== expected ? console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+    : console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+};
+
+const countLetters = function(sentence) {
+  const lowerSentence = sentence.toLowerCase()
+  let count = {};
+    for (const letter of lowerSentence) {
+    if (letter !== " ") {
+      count[letter] = count[letter] +1 || 1
+    }
+  }
+  return count
+}
+
+console.log(countLetters("Jenny"))
+console.log(countLetters("Jenny is cool"))
+
+//this fails because it is supposed to compaire primitive data types
+assertEqual(countLetters("Jenny"), {j: 1, e: 1, n: 2, y: 1})
+assertEqual(countLetters("Jenny is cool"), { j: 1, e: 1, n: 2, y: 1, i: 1, s: 1, c: 1, o: 2, l: 1 })
