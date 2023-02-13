d42a45a025cc2fae8f400234fff59a9657112542
Jenny Carroll <jennygc@telus.net>
2023-02-13 23:22:51 +0000

    I refactored tailTest and middleTest to utilize chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,22 @@
+const assert = require("chai").assert;
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
 
-//TESTS
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(["5"]), []);
+  });
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), [
+      "Lighthouse",
+      "Labs",
+    ]);
+  });
+  it("does not modify the original array", () => {
+    let testArray = ["test", "array"];
+    assert.deepEqual(tail(testArray), ["array"]);
+    assert.strictEqual(testArray.length, 2);
+  });
+});
