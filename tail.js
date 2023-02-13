7a05bfababa8ae6dafab21765ff13f7530d93a1f
Jenny Carroll <jennygc@telus.net>
2023-02-13 17:16:25 +0000

    I created a file tailTest.js to house my tail tests and I made sure it required the correct functions to operate.  I removed the test code from tail.js and exported it to moddule.exports

--- tail.js
+++ tail.js
@@ -1,12 +1,6 @@
-const assertEqual = function(actual, expected) {
-  actual !== expected ? console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
-    : console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-};
- 
+const assertEqual = require("./assertEqual");
 
-  
-
-const tail = function(array) {
+const tail = function (array) {
   const newArray = [];
   for (let i = 1; i < array.length; ++i) {
     newArray.push(array[i]);
@@ -14,9 +8,4 @@ const tail = function(array) {
   return newArray;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-console.log(tail([1]));
-console.log(tail([]));
+module.exports = tail;
