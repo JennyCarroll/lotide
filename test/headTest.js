0d084e88ee1debb962180f484b1372f6366bdf5e
Jenny Carroll <jennygc@telus.net>
2023-02-13 22:41:39 +0000

    I finished creating tests using chai assertions rather than my own tests

--- test/headTest.js
+++ test/headTest.js
@@ -8,10 +8,13 @@ describe("#head", () => {
   it("returns '5' for ['5']", () => {
     assert.strictEqual(head(["5"]), "5");
   });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+  it("returns 'undefined' for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  it("returns 1 for [1]", () => {
+    assert.strictEqual(head([1]), 1);
+  });
 });
-
-//TESTS
-// assertEqual(head([5, 6, 7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([]), undefined);
-// assertEqual(head([1]), 1);
